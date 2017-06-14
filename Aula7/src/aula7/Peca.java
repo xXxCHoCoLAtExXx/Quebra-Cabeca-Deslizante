bb10423d863bc567ff3e4b07ea4d5d907463c6ca
chocolate <alvesjclaudio@hotmail.com>
2017-06-14 00:43:50 -0300

    Ainda mudando (não pronto)

---
+++ Aula7/src/aula7/Peca.java
@@ -0,0 +1,15 @@
+package Aula7;
+
+public class Peca {
+
+    protected String peca;
+
+    public String getPeca() {
+        return this.peca;
+    }
+
+    public void setPeca(int i) {
+        this.peca = Integer.toString(i)+"";
+    
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Quebra-Cabeca-Deslizante
-Jogo de quebra cabeça, atividade orientação a objeto da faculdade.
